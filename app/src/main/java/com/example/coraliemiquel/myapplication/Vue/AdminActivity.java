3c8d2695649b2a78728f6c0f196b57818e9028bf
Coralie Miquel <Coralie Miquel>
2016-11-03 13:17:40 +0100

    MAJ interface Admin - code toujoursbugger

--- app/src/main/java/com/example/coraliemiquel/myapplication/Vue/AdminActivity.java
+++ app/src/main/java/com/example/coraliemiquel/myapplication/Vue/AdminActivity.java
@@ -1,7 +1,9 @@
 package com.example.coraliemiquel.myapplication.Vue;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 /**
  * Created by Coralie MIQUEL on 03/11/2016.
@@ -12,5 +14,13 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_activity);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = new Intent(AdminActivity.this, ConnectActivity.class);
+        startActivity(intent);
+        return  super.onOptionsItemSelected(item);
     }
 }
